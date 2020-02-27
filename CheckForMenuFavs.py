24852c629cfb06d6e4d2eddd8b1a9a50c9e055d8
Summer G Banister <sgb7@humboldt.edu>
2020-02-27 15:25:00 -0800

    Edited code

--- CheckForMenuFavs.py
+++ CheckForMenuFavs.py
@@ -8,27 +8,18 @@ import csv
 import shutil
 import sqlite3
 
-#test on school computer
+menu_url = "https://dining.humboldt.edu/j-menu"
+response = urllib.request.urlopen(menu_url)
+html = response.read()
+soup = BeautifulSoup(html, "html.parser")
 
-def get_menu_items():
-    menu_url = "https://dining.humboldt.edu/j-menu"
-    response = urllib.request.urlopen(menu_url)
-    html = response.read()
-    soup = BeautifulSoup(html, "html.parser")
-
-    name_box = soup.find('div', attrs={'class' : 'food-menu-dropdown'})
-    name = name_box.text.strip()
+name_box = soup.find('div', attrs={'class' : 'food-menu-dropdown'})
+name = name_box.text.strip()
 
 
-favorites = ["Belgian Waffles", "Pasta Bar", "Baklava", "Cheesy Potatoes", "Veggie Omelet", "Beef Barbocoa"]
+favorites = ["Belgian Waffles", "Pasta Bar", "Baklava", "Cheesy Potatoes", "Scrambled Eggs", "Beef Barbocoa", "French fries"]
 
-def favorite_alert():
-    print("Favorites available today: ")
-    for fav in favorites:
-        if fav in name:
-            print(fav)
-    
-
-if __name__ == '__main__':
-    get_menu_items()
-    favorite_alert()
+print("Favorites available today: ")
+for fav in favorites:
+    if fav in name:
+        print(" " + fav)
